67d971a549ce336bc45c2b836a6bb6b8a25773dc
Gernot Raudner <gitkraken@gernotraudner.at>
2019-07-03 22:51:41 +0200

    api key template

---
+++ src/api-constants.template.js
@@ -0,0 +1,3 @@
+// insert auth tokens and rename this file to api-constants.js
+export const MAPBOX_GL_TOKEN = 'your auth token here';
+export const OPEN_CAGE_DATA_AUTH_TOKEN = 'your auth token here';
